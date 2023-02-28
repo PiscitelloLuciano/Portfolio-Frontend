3955cc5c4ee693acba1d2c188415b83486e74693
Luciano Piscitello <lucianopiscitello8@gmail.com>
2023-02-28 14:18:28 -0300

    Pasamos html css a proyecto Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/index/header/header.component';
+import { IndexComponent } from './components/index/index.component';
+import { FormularioComponent } from './components/login/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: IndexComponent },
+  { path: 'login', component: FormularioComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './components/login/formulario/formulario.component';
+import { BienvenidaComponent } from './components/login/bienvenida/bienvenida.component';
+import { HeaderComponent } from './components/index/header/header.component';
+import { InfoComponent } from './components/index/info/info.component';
+import { SkillsComponent } from './components/index/skills/skills.component';
+import { ProjectsComponent } from './components/index/projects/projects.component';
+import { FooterComponent } from './components/index/footer/footer.component';
+import { IndexComponent } from './components/index/index.component';
+import { DetailsComponent } from './components/index/details/details.component';
+import { BtnEditComponent } from './components/reusable/btn-edit/btn-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    BienvenidaComponent,
+    HeaderComponent,
+    InfoComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent,
+    IndexComponent,
+    DetailsComponent,
+    BtnEditComponent
   ],
   imports: [
     BrowserModule,
